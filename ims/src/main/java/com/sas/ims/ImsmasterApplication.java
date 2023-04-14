c8e0540c6f5dfaef9a0a8d82520aa0c800bd3b22
Lakshay <Lakshay@sastechstudio.com>
2023-04-14 12:32:23 +0530

    Vendorupdate api(Ims-master).

--- ims/src/main/java/com/sas/ims/ImsmasterApplication.java
+++ ims/src/main/java/com/sas/ims/ImsmasterApplication.java
@@ -4,6 +4,7 @@ import com.sas.tokenlib.config.CommonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 @Import(CommonConfig.class)
+@EnableJpaRepositories("com.sas.ims")
 public class ImsmasterApplication {
 
 	public static void main(String[] args) {
--- ims/src/main/java/com/sas/ims/repository/VendorMasterRepository.java
+++ ims/src/main/java/com/sas/ims/repository/VendorMasterRepository.java
@@ -4,9 +4,18 @@ import com.sas.ims.entity.VendorMaster;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface VendorMasterRepository extends JpaRepository<VendorMaster, Long> {
     Page<VendorMaster> findByOrgId(Long orgId, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update vendor_master set vendor_status='X' where org_id =:companyId and vendor_id =:vendorId",nativeQuery = true)
+    void updateVendorDetails(Long companyId, Long vendorId);
 }
